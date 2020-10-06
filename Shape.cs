2e44dc821f112e37be55dd5f3f4952e6df6c5bdb
scov1 <seda8888-92@mail.ru>
2020-10-06 16:02:35 +0600

    Add files via upload

---
+++ Shape.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Shape
+{
+    public static class MyClass
+    {
+
+
+        public static double AreaSquare(double side)
+        {
+            return side * side;
+        }
+
+
+        public static double AreaRectangle(double lenght,double width)
+        {
+            return lenght * width;
+        }
+
+        public static double AreaTreangle(double sideBase,double height)
+        {
+            return sideBase * height/2;
+        }
+    }
+}
