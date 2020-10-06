578750b7b7008d760b3ee961c7c270407637ce1e
seda <seda@MacBook-Pro-seda.local>
2020-10-06 16:34:57 +0600

    less

---
+++ Lesson3/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Threading;
+
+namespace Lesson3
+{
+    class MainClass
+    {
+
+        public static void Main(string[] args)
+        {
+
+            Console.Write("Vvedite kolichestvo chisel: ");
+            Thread thread = new Thread(new ParameterizedThreadStart(FibonaciParam));
+            int inputNumber = Convert.ToInt32(Console.ReadLine());
+
+  
+
+          
+             thread.Start(inputNumber);
+
+
+            Console.WriteLine("Najmite klaviwu dlya ostanovki");
+            Console.ReadKey();
+            thread.Abort();
+            Console.WriteLine("\nPotok ostanovlen");
+
+
+
+
+            Console.ReadLine();
+        }
+
+
+        public static void FibonaciParam(object param)
+        {
+
+            int j = 1;
+            int num = (int)param;
+
+            if (num == 0)
+            {
+                Console.Write("Neverniy diapozon");
+            }
+
+            for (int i = 1; i <= num; i += j)
+            {
+                if (num == 1)
+                {
+                    Console.Write(1);
+                }
+                else
+                {
+                    Console.Write("{0} ", i);
+                    j = i - j;
+                }
+
+                Thread.Sleep(1000);
+
+            }
+        }
+    }
+}
