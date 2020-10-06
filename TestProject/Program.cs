268c294cf27e12538c44111e01f11a0daf94dbcc
seda <seda@MacBook-Pro-seda.local>
2020-10-06 15:46:18 +0600

    less2

---
+++ TestProject/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TestProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double result = Shape.MyClass.AreaSquare(5);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
