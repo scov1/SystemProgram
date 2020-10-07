08b0665919beab13dbd676511b8db9eb8db90b3b
seda <seda@MacBook-Pro-seda.local>
2020-10-07 12:31:35 +0600

    less

---
+++ HomeWork6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Diagnostics;
+using System.Windows;
+
+namespace HomeWork6
+{
+	class Program
+	{
+
+		static void Main(string[] args)
+		{
+			if (Process.GetProcessesByName("HomeWork6").Length > 3)
+			{
+				Console.WriteLine("You cannot create more than 3 copies");
+			}
+            else
+            {
+				Console.WriteLine("Working!");
+            }
+
+			Console.ReadKey();
+		}
+	}
+}
