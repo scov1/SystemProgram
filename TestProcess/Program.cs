6887af9a1442ca0a7ca157a231e9a70ead64046c
seda <seda@MacBook-Pro-seda.local>
2020-10-13 14:56:42 +0600

    less

---
+++ TestProcess/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProcess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+ 
+            ProcessStartInfo process1 = new ProcessStartInfo();
+            process1.FileName = "C:/Users/seda8/Source/Repos/TestProcess/ProcessHomeWork/bin/Debug/ProcessHomeWork.exe";
+            process1.Arguments = "6 2 *";
+            Process.Start(process1);
+
+            Console.ReadLine();
+        }
+    }
+}
