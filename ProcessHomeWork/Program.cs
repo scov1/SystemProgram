05508947eb8524ca4189c97f983f06b1866a444c
seda <seda@MacBook-Pro-seda.local>
2020-10-13 14:58:57 +0600

    less+

---
+++ ProcessHomeWork/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessHomeWork
+{
+    class Program
+    {
+        static void Main(string[] args) 
+
+        {
+                var num1 = args[0];
+                var num2 = args[1];
+                var checkOperation = args[2];
+           
+
+                if (checkOperation == "+")
+                {
+                    int res = Convert.ToInt32(num1) + Convert.ToInt32(num2);
+                    Console.WriteLine($"{num1} + {num2} = " + res );
+                }
+                else if (checkOperation == "-")
+                {
+                    int res = Convert.ToInt32(num1) - Convert.ToInt32(num2);
+                    Console.WriteLine($"{num1} - {num2} = " + res);
+                }
+                else if (checkOperation == "/")
+                {
+                    int res = Convert.ToInt32(num1) / Convert.ToInt32(num2);
+                    Console.WriteLine($"{num1} / {num2} = " + res);
+                }
+                else if (checkOperation == "*")
+                {
+                    int res = Convert.ToInt32(num1) * Convert.ToInt32(num2);
+                    Console.WriteLine($"{num1} * {num2} = " + res);
+                }
+            Console.ReadLine();
+        }
+    }
+}
