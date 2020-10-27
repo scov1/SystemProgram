4c080cff29b997d2185012c0eca5410a3848e371
seda8888-92 <seda8888-92@mail.ru>
2020-10-27 15:15:58 +0600

    lessSocket

---
+++ Client/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			const string ip = "192.168.102.23";
+			const int port = 8080;
+
+			IPEndPoint ep = new IPEndPoint(IPAddress.Parse(ip), port);
+			Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+
+			Console.WriteLine("Input str:");
+			string str = Console.ReadLine();
+
+			var data = Encoding.UTF8.GetBytes(str);
+			socket.Connect(ep);
+			socket.Send(data);
+
+			byte[] buffer = new byte[1024];
+			StringBuilder sbAnswer = new StringBuilder();
+			int i=0;
+			do
+			{
+				i = socket.Receive(buffer);
+				sbAnswer.Append(Encoding.UTF8.GetString(buffer, 0, i));
+			} while (socket.Available > 0);
+
+			Console.WriteLine(sbAnswer.ToString());
+
+			socket.Shutdown(SocketShutdown.Both);
+			socket.Close();
+
+
+			Console.ReadLine();
+		}
+	}
+}
