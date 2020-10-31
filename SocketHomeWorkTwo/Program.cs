50c0c2d14e1d2bf22ee677d6fec327fa49840535
seda8888-92 <seda8888-92@mail.ru>
2020-10-31 12:43:16 +0600

    lessLimit

--- SocketHomeWorkTwo/Program.cs
+++ SocketHomeWorkTwo/Program.cs
@@ -12,9 +12,13 @@ namespace Server
 	{
         private const string IP = "127.0.0.1";
         private const int Port = 12000;
+        static readonly byte[] CONN_STOP = Encoding.ASCII.GetBytes("Server is busy, please try later :(");
         static void Main(string[] args)
 		{
+            int conn = 0;
             TcpListener server = null;
+            Console.WriteLine("Input max connects client");
+            int count = Convert.ToInt32(Console.ReadLine());
             try
             {
                 server = new TcpListener(IPAddress.Parse(IP), Port);
@@ -22,20 +26,40 @@ namespace Server
                 while (true)
                 {
                     Console.WriteLine("Waiting...");
-            
                     TcpClient client = server.AcceptTcpClient();
-                    Console.WriteLine("Connection with Client");
-                    Console.WriteLine("Connection was time => " + DateTime.Now);
-                    NetworkStream stream = client.GetStream();
-                    string response = "Currency your money " ;
-                    byte[] data = Encoding.UTF8.GetBytes(response);
-                    stream.Write(data, 0, data.Length);
 
-                    Console.WriteLine($"Sent Message: {response}");
-       
-                    stream.Close();
-                    client.Close();
-                    
+                    if (conn >= count)
+                    {
+                        //   TcpClient client = server.AcceptTcpClient();
+                        NetworkStream stream = client.GetStream();
+                        string response = "Server is busy, please try later :(";
+                        byte[] data = Encoding.UTF8.GetBytes(response);
+                        stream.Write(data, 0, data.Length);
+                        Console.WriteLine($"Sent Message: {response}");
+                        //string response = "Server is busy,please try later :(";
+                        //byte[] data = Encoding.UTF8.GetBytes(response);
+                        //stream.Write(data, 0, data.Length);
+                        //Console.WriteLine($"Sent Message: {response}");
+                        //stream.Close();
+                        stream.Close();
+                        client.Close();
+
+                    }
+                    else
+                    {
+                      
+                        Console.WriteLine("Connection with Client");
+                        Console.WriteLine("Connection was time => " + DateTime.Now);
+                        conn++;
+                        NetworkStream stream = client.GetStream();
+                        string response = "Everythings it's all ok";
+                        byte[] data = Encoding.UTF8.GetBytes(response);
+                        stream.Write(data, 0, data.Length);
+                        Console.WriteLine($"Sent Message: {response}");
+                        stream.Close();
+                        client.Close();
+                    }
+
                 }
             }
             catch (Exception e)
